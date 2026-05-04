a0c38d063bd27221edc53b303cad4d84d1add0e3
Johnmark <wamukotajohnmark@gmail.com>
2026-05-04 10:45:08 +0700

    fix login and regiter ui

--- update.bat
+++ update.bat
@@ -1,6 +1,6 @@
 @echo off
 git add .
-git commit -m "🔥 Auto-update: %date% %time%"
+git commit -m "fix login and regiter ui"
 git pull origin main --rebase
 git push origin main
 echo ✅ Update complete! Check your website in a few minutes. 🚀
