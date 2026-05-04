436f00d199f4672cd40c1e1f78ad68a07307869c
Johnmark <wamukotajohnmark@gmail.com>
2026-05-04 17:57:29 +0700

    fix meta tags

--- update.bat
+++ update.bat
@@ -1,6 +1,6 @@
 @echo off
 git add .
-git commit -m "fix login and regiter ui"
+git commit -m "fix meta tags"
 git pull origin main --rebase
 git push origin main
 echo ✅ Update complete! Check your website in a few minutes. 🚀
