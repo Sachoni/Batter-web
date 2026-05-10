9228dd76c9c4d665be6fead35768540b894c256f
Johnmark <wamukotajohnmark@gmail.com>
2026-05-10 16:16:24 +0700

    fix account login

--- account/firebase.js
+++ account/firebase.js
@@ -1,19 +1,17 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// 🔥 Your Firebase Config
 const firebaseConfig = {
-  apiKey: "AIzaSyCNKw13mRo1IWmKaJ-lNy7vuPXRaIcuZWs",
-  authDomain: "tradesystem-ada4d.firebaseapp.com",
-  projectId: "tradesystem-ada4d",
-  storageBucket: "tradesystem-ada4d.firebasestorage.app",
-  messagingSenderId: "557803563021",
-  appId: "1:557803563021:web:2c27a9c8b660952da24d44",
+  apiKey: "AIzaSyCFxptO2uFBcgIxLybnQ8FjDDRMcKl8lpw",
+  authDomain: "batter-app-44eaf.firebaseapp.com",
+  projectId: "batter-app-44eaf",
+  storageBucket: "batter-app-44eaf.firebasestorage.app",
+  messagingSenderId: "709751116872",
+  appId: "1:709751116872:web:291c8b21604f3ad3bff47d"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 
-export { auth };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
--- update.bat
+++ update.bat
@@ -1,6 +1,6 @@
 @echo off
 git add .
-git commit -m "fix meta tags"
+git commit -m "fix account login"
 git pull origin main --rebase
 git push origin main
 echo ✅ Update complete! Check your website in a few minutes. 🚀
