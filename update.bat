b14a9521ede188048b09e833734ffcb4e09b8cf6
Johnmark <wamukotajohnmark@gmail.com>
2026-06-01 12:08:27 +0700

    screen addition

--- update.bat
+++ update.bat
@@ -1,6 +1,6 @@
 @echo off
 git add .
-git commit -m "fix account login"
+git commit -m "screen addition"
 git pull origin main --rebase
 git push origin main
 echo ✅ Update complete! Check your website in a few minutes. 🚀
